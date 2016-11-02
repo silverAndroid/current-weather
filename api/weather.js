ddbae3a360fbbdf742ade32830c06e52b7b14dad
Rushil Perera <pererarushil1940@gmail.com>
2016-11-02 19:51:54 -0400

    Added controller logic to get current weather

--- api/weather.js
+++ api/weather.js
@@ -5,11 +5,12 @@ var request = require('request');
 var apiKey = process.env.API_KEY;
 
 var getWeather = function (latitude, longitude, callback) {
-    request(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`, function (error, response, body) {
-        var json = {status: response.statusCode};
-        json[!error ? 'weatherData' : 'error'] = JSON.parse(body);
-        callback(json);
-    });
+    request(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`,
+        function (error, response, body) {
+            var json = {status: response.statusCode};
+            json[!error ? 'weatherData' : 'error'] = JSON.parse(body);
+            callback(json);
+        });
 };
 
 module.exports = {
